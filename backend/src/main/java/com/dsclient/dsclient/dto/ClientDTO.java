32a1162b9d382e7eb441009328dcd963d3cc5c04
Thrysk <guicafure@gmail.com>
2021-09-23 20:01:38 -0400

    crud completo

--- backend/src/main/java/com/dsclient/dsclient/dto/ClientDTO.java
+++ backend/src/main/java/com/dsclient/dsclient/dto/ClientDTO.java
@@ -1,5 +1,7 @@
 package com.dsclient.dsclient.dto;
 
+import java.time.Instant;
+
 import com.dsclient.dsclient.entities.Client;
 
 public class ClientDTO {
@@ -8,25 +10,28 @@ public class ClientDTO {
 	private String name;
 	private String cpf;
 	private Double income;
+	private Instant birthDate;
 	private Integer children;
-	
+
 	public ClientDTO() {
-	
+
 	}
-	
-	public ClientDTO(Long id, String name, String cpf, Double income, Integer children) {
+
+	public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
 		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.income = income;
+		this.birthDate = birthDate;
 		this.children = children;
 	}
-	
+
 	public ClientDTO(Client entity) {
 		this.id = entity.getId();
 		this.name = entity.getName();
 		this.cpf = entity.getCpf();
 		this.income = entity.getIncome();
+		this.birthDate = entity.getBirthDate();
 		this.children = entity.getChildren();
 	}
 
@@ -62,6 +67,14 @@ public class ClientDTO {
 		this.income = income;
 	}
 
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
 	public Integer getChildren() {
 		return children;
 	}
@@ -69,7 +82,5 @@ public class ClientDTO {
 	public void setChildren(Integer children) {
 		this.children = children;
 	}
-	
-	
-	
+
 }
--- backend/src/main/java/com/dsclient/dsclient/services/ClientService.java
+++ backend/src/main/java/com/dsclient/dsclient/services/ClientService.java
@@ -2,7 +2,10 @@ package com.dsclient.dsclient.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -18,19 +21,57 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
-		
+
 		return list.map(x -> new ClientDTO(x));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("ID não encontrado!"));
-		
+
 		return new ClientDTO(entity);
 	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID não encontrado: " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID não encontrado: " + id);
+		}
+	}
+	
+	
 }
